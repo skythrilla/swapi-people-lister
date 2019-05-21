10c7cf9e2f30cf35ca0768192c9ad1aab0747cbd
Schuyler Lawrence <schuyler.lawrence@gmail.com>
2019-05-20 22:15:21 -0700

    2

--- main.js
+++ main.js
@@ -1,52 +1,34 @@
 const divleft = document.querySelector("#left");
 const divright = document.querySelector("#right");
 // const divwayright = document.querySelector("#wayright");
-let selection = '';
+const arr = [];
+const arr2 = [];
 async function peeps() {
     try {
         const result = await fetch('https://swapi.co/api/people/');
         const data = await result.json();
 
         const people =
-            `
-                <ul>${
-                (data.results).map(key => `<li>${key.name}</li>`).join('')
+            `<ul>${
+                (data.results).map(key => arr.push(`<li>${key.name}</li>`))
                 }
                 </ul>`;
+        console.log(arr);
 
-        divleft.innerHTML = people;
-        // let choice=${key.name};
-        // divleft.innerHTML = data.results[0].name;
-        // console.log(data.results);
+        divleft.innerHTML = arr.join('');
 
         divleft.addEventListener("click", ev => {
-            // const action = ev.target.document.getElementsByTagName("divleft");
-            // console.log(action);
-            // divleft.innerHTML = people;
-            selection = ev.target;
-            console.log(selection);
-            console.log(ev.target);
-            console.log(people[3]);
 
-            for (let i = 0; i < people.length; i++) {
-                if (selection === people[i]) {
-                    selection = people[i];
-                    console.log(people[i]);
+            console.log(ev.target);
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === ev.target) {
+                    arr2.push(arr[i]);
                 }
+                // console.log(arr[i]);
             }
             // console.log(selection);
 
-            // const html = `
-            //     <ul>${
-            //     (data.results).map(key => `<li>${key}</li>`).join('')
-            //     }
-            //     </ul>`;
-
-            // const obj = data.results[0];
-            // const html =
-            divright.innerHTML = selection;
-
-            // divwayright.innerHTML = 'hellllllo';
+            divright.innerHTML = ev.target;
             // const html = `<ul><${side}</h3><h3 class = '${isZero && "zero"}'>${amount}</h3></center>`;
         });
     } catch (error) {
