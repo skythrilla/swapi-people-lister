e26b9c98615ebca76d47807cd180b55ceaf69a48
Schuyler Lawrence <schuyler.lawrence@gmail.com>
2019-05-20 23:52:42 -0700

    3

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const divleft = document.querySelector("#left");
 const divright = document.querySelector("#right");
 // const divwayright = document.querySelector("#wayright");
-const arr = [];
-const arr2 = [];
 async function peeps() {
     try {
         const result = await fetch('https://swapi.co/api/people/');
@@ -10,25 +8,29 @@ async function peeps() {
 
         const people =
             `<ul>${
-                (data.results).map(key => arr.push(`<li>${key.name}</li>`))
+                (data.results).map(key => `<li data-action='yo'>${key.name}</li>`).join('')
                 }
                 </ul>`;
-        console.log(arr);
 
-        divleft.innerHTML = arr.join('');
+        divleft.innerHTML = people;
 
         divleft.addEventListener("click", ev => {
-
             console.log(ev.target);
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[i] === ev.target) {
-                    arr2.push(arr[i]);
-                }
-                // console.log(arr[i]);
-            }
-            // console.log(selection);
 
-            divright.innerHTML = ev.target;
+            const action = ev.target.getAttribute("yo");
+            divright.innerHTML = data.results[0].name;
+
+            // if (action === "yo")
+            //     divright.innerHTML = ev.target;
+            // console.log(arr2);
+
+            // console.log(ev.target.action);
+            // for (let i = 0; i < arr.length; i++) {
+            //     if (arr[i] === ev.target) {
+            //         arr2.push(arr[i]);
+            //     }
+            // }
+            // divright.innerHTML = action;
             // const html = `<ul><${side}</h3><h3 class = '${isZero && "zero"}'>${amount}</h3></center>`;
         });
     } catch (error) {
